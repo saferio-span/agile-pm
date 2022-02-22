e56d0021d5b74d4d4a8b218efabeb4f01b897396
stsjohnsaferio <stsjohnsaferio@dotnetethics.com>
2022-02-22 12:30:41 +0530

    Fixed logout bug and edit user bug

--- pages/users/add.js
+++ pages/users/add.js
@@ -33,7 +33,7 @@ const Add = (props) => {
     const [userDetails,setUserDetails] = useState({
         email:"",
         name:"",
-        mobile:"",
+        phone:"",
         globalAccess : false,
         role:"",
         previlage:""
@@ -89,7 +89,7 @@ const Add = (props) => {
                     setUserDetails({
                         email:"",
                         name:"",
-                        mobile:"",
+                        phone:"",
                         globalAccess : false,
                         role:"",
                         previlage:""
@@ -190,7 +190,7 @@ const Add = (props) => {
                         </div>
                         <div className='mt-2'>
                             <FormLabel htmlFor='email'>Mobile Number<span className='text-danger'>*</span></FormLabel>
-                            <Input id='mobileNumber' name="mobile" type='number' maxLength="10" minLength="10" onChange={handleInputChange} />
+                            <Input id='mobileNumber' name="phone" type='number' maxLength="10" minLength="10" onChange={handleInputChange} />
                         </div>
 
                         <Text fontSize='2xl' className='mt-3'>Scope</Text>
